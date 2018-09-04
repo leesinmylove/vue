9615414c4d7121776a32a6a51317110781609cbb
HuangZeFeng(桌面事业部_测试部) <huangzefeng@sogou-inc.com>
2018-09-04 09:55:42 +0800

    myvue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <span>hello red</span>
+    <div class="msg">{{msg}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['srcs'],
+  data () {
+    return {
+      msg: 'hello yellow'
+    }
+  },
+  computed: {
+  },
+  watch: {
+  },
+  created () {
+
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.hello {
+  color: red;
+  .msg {
+    color: aqua;
+  }
+}
+</style>
